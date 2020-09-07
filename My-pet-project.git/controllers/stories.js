1b968391a288e923f02d5911c6c3d3ad33e4e7ae
Q2kPetrov <ivaylozp@gmail.com>
2020-09-07 23:32:24 +0300

    Add story to the db

--- My-pet-project.git/controllers/stories.js
+++ My-pet-project.git/controllers/stories.js
@@ -1,4 +1,5 @@
 import extend from '../utils/context.js';
+import models from '../models/index.js';
 import checkForUser from '../utils/checkForUser.js';
 
 export default {
@@ -16,11 +17,82 @@ export default {
             displayUserName(context);
             extend(context).then(function () {
                 this.partial('../views/sections/create-story.hbs')
-                .then(listenForUploadedPictures)
+                    .then(listenForUploadedPictures)
             })
+        }
+    },
+    post: {
+        create(context) {
+            checkForUser(context)
+            // displayUserName(context);
+            // extend(context)
+            const user = firebase.auth().currentUser;
+
+            // // These params come from the create-story.hbs template (name). Their name should be the same here, otherwise Sammy throws an exception
+            let imagesRef = document.querySelector('#upload-story-images');
+            const { title, description, email, phonenumber, storyImages } = context.params;
+            // const storageDestination = firebase.storage().ref('users/' + user.uid + '/' + title);
+
+            //Check if there are any images uploaded
+            if (imagesRef.files.length > 0) {
+                for (var i = 0; i < imagesRef.files.length; i++) {
+                    var imageFile = imagesRef.files[i];
+                    uploadImageAsPromise(imageFile, context, user).then((res) => {
+                        console.log(res);
+                    });
+                }
+            }
+
+            console.log(user);
+            //We create new story and save it in the firestore
+            const data = {
+                ...context.params,
+                uid: context.userId,
+                authorName: user.displayName === null ? "Too shy to share that" : user.displayName,
+                likes: 0,
+                comments: []
+            }
+            
+            models.story.create(data)
+                .then(response => {
+                    context.redirect('#/home');
+                })
+                .catch(e => alert(e.message));
+
         }
     }
 
+}
+
+async function uploadImageAsPromise(imageFile, context, user) {
+    return new Promise(function (resolve, reject) {
+
+
+        // These params come from the create-story.hbs template (name). Their name should be the same here, otherwise Sammy throws an exception
+
+        const { title, description, email, phonenumber, storyImages } = context.params;
+
+        let storageDestination = firebase.storage().ref('users/' + user.uid + '/' + title + '/' + imageFile.name);
+
+        var task = storageDestination.put(imageFile);
+        console.log(task);
+        //Update progress bar
+        //TODO - render the progress
+        task.on('state_changed',
+            function progress(snapshot) {
+                var percentage = snapshot.bytesTransferred / snapshot.totalBytes *
+                    100;
+            },
+            function error(err) {
+                console.log(err);
+                reject(err);
+            },
+            function complete() {
+                var downloadURL = task.snapshot.downloadURL;
+                resolve(downloadURL);
+            }
+        );
+    });
 }
 
 function displayUserName(context) {
--- My-pet-project.git/models/index.js
+++ My-pet-project.git/models/index.js
@@ -1,5 +1,7 @@
 import user from './user.js';
+import story from './story.js';
 
 export default {
-    user
+    user,
+    story
 };
